e1c1b1a8f27117e9bea7ddb22f56be25d23ee7ed
HacheG <943@holbertonschool.com>
2020-06-20 12:02:32 -0500

    [INIT] create directory

---
+++ 0x06-unity-assets_ui/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinTrigger : MonoBehaviour
+{
+    public GameObject player;
+    public Text timerText;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            player.GetComponent<Timer>().enabled = false;
+            timerText.color = Color.green;
+            timerText.fontSize = 60;
+            
+            Debug.Log("enter");
+        }
+    }
+}
---
+++ tester/Assets/test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    private GameObject cube1;
+    public float dt, ts;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    private void Awake()
+    {
+        /*cube1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube1.transform.position = new Vector3(0, 1, 0);
+        cube1.GetComponent<Renderer>().material.color = Color.blue;*/
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        dt = Time.deltaTime;
+        Debug.Log(Time.timeScale*2);
+        //transform.Translate(0, 1, 0);
+    }
+}
