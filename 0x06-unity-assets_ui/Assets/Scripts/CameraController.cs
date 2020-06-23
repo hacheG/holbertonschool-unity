b354af4e698c6e88c978e16e0219ece7a6717104
HacheG <943@holbertonschool.com>
2020-06-23 01:04:22 -0500

    [ADD] all tasks

--- 0x06-unity-assets_ui/Assets/Scripts/CameraController.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/CameraController.cs
@@ -10,6 +10,8 @@ public class CameraController : MonoBehaviour
     public GameObject player;
     public float turnSpeed = 5.0f;
 
+    public bool isInverted;
+
     // Start is called before the first frame update
     void Start()
     {
