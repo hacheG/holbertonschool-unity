5c6d9587516229c7c019b0e15c926f544eed7c8d
HacheG <943@holbertonschool.com>
2020-11-06 23:41:24 -0500

    firstAR

---
+++ 0x09-unity_ar_business_card/Assets/Scripts/DesencadenarAnim.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class DesencadenarAnim : MonoBehaviour
+{
+    public Animator stickerAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stickerAnimator.SetTrigger("Play");
+    }
+}
---
+++ 0x09-unity_ar_business_card/Assets/Scripts/SocialNetwork.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SocialNetwork : MonoBehaviour
+{
+
+    public string gitHub = "https://github.com/hacheG";
+    public string face = "https://www.facebook.com/hugo.trujillo.hache/";
+    public string twiter = "https://twitter.com/HugoATrujillo";
+    public string insta = "https://www.instagram.com/hachetrescomacatorce/";
+
+    // Start is called before the first frame update
+    public void OpenGit()
+    {
+        Application.OpenURL(gitHub);
+    }
+
+    public void OpenFace()
+    {
+        Application.OpenURL(face);
+    }
+
+    public void OpenTwiter()
+    {
+        Application.OpenURL(twiter);
+    }
+
+    public void OpenInsta()
+    {
+        Application.OpenURL(insta);
+    }
+
+}
